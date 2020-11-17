fa10cdf4e07acda420f140588e9390b33de558c7
parin34 <72636755+parin34@users.noreply.github.com>
2020-11-16 20:15:04 -0600

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,25 @@
+class Chain{
+
+    constructor(body1,body2){
+
+    var options={
+        bodyA:body1,
+        bodyB:body2,
+        stiffness:0.04,
+        length:10
+    }
+    this.chain=Constraint.create(options)
+    World.add(world,this.chain);
+    }
+
+    display(){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+        push();
+        strokeWeight(4);
+        stroke("red")
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+        pop();
+    }
+
+}
